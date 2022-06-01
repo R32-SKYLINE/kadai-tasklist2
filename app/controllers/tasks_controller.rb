be5ed597b37e84701378fefedbb2aa01b7b4bc02
m.mitsui <mm.techacademy@gmail.com>
2022-06-01 13:28:16 +0000

    kadai 2 Submission

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,22 @@
 class TasksController < ApplicationController
    def index
-      @messages = Task.all
+      @works = Task.all
    end
 
    def show
-      @message = Task.find(params[:id])
+      @work = Task.find(params[:id])
    end
 
    def new
-      @message = Task.new
+      @work = Task.new
    end
 
    def create
-      @message = Task.new(message_params)
+      @work = Task.new(message_params)
       
-      if @message.save
+      if @work.save
          flash[:success] = 'タスクが正常に登録できました。'
-         redirect_to @message
+         redirect_to @work
       else
          flash.now[:danger] = 'タスクが登録出来ませんでした。'
          render :new
@@ -24,15 +24,15 @@ class TasksController < ApplicationController
    end
 
    def edit
-      @message = Task.find(params[:id])
+      @work = Task.find(params[:id])
    end
 
    def update
-      @message = Task.find(params[:id])
+      @work = Task.find(params[:id])
       
-      if @message.update(message_params)
+      if @work.update(message_params)
          flash[:success] = 'タスクが正常に更新されました。'
-         redirect_to @message
+         redirect_to @work
       else
          flash.now[:danger] = 'タスクが更新されませんでした。'
          render :new
@@ -40,17 +40,17 @@ class TasksController < ApplicationController
    end
 
    def destroy
-      @message = Task.find(params[:id])
-      @message.destroy
+      @work = Task.find(params[:id])
+      @work.destroy
       
       flash[:success] = 'タスクは正常に削除されました。'
       redirect_to tasks_url
    end
-end
-
-private
-
-# Strong Parameter
-def message_params
-   params.require(:task).permit(:content)
+   
+   private
+   
+   # Strong Parameter
+   def message_params
+      params.require(:task).permit(:content)
+   end
 end
